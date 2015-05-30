012d9a84f9949e92ed947b774491d3bc1a6023e5
Andrei Visan <andreivisan@Andreis-MBP.home>
2015-05-30 21:58:15 +0200

    finished main design

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/main-dashboard', function(req, res) {
+    res.render('dashboard_main');
+});
+
 module.exports = router;

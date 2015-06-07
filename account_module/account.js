fb77b49c4812b3803bb8ea5d35bc4af843739fa7
Andrei Visan <andreivisan@Andreis-MBP.home>
2015-06-07 21:58:35 +0200

    added account DAO

---
+++ account_module/account.js
@@ -0,0 +1,43 @@
+/**
+ * Created by andreivisan on 6/7/15.
+ */
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.MYSQL_URL,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: 'heroku_e56800c88b7a6b4',
+    debug: false
+});
+
+
+module.exports.createAccount = function(request, callback) {
+    var account = {
+        financial_institution: request.body.financial_institution,
+        name: request.body.name,
+        currency: request.body.currency,
+        type: request.body.type
+    };
+
+    pool.getConnection(function(err, connection) {
+        if(err) {
+            connection.release();
+            callback({"code" : 100, "status": "Error in connection database"}, null);
+            return;
+        }
+
+        console.log("ACCOUNT: connected as id " + connection.threadId);
+
+        connection.query("INSERT INTO account SET ?", account, function(err, result) {
+            connection.release();
+            callback(err, result);
+        });
+
+        connection.on("error", function(err) {
+            callback({"code" : 100, "status": "Error in connection database"}, null);
+            return;
+        });
+    });
+}
